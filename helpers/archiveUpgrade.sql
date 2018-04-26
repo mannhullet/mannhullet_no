feee1ca003a68c73809967a096b1f4607d9fdc50
Andreas Bro Kolstø <andreas.kolsto@gmail.com>
2018-04-26 12:53:02 +0200

    Merged the files needed to transiton the database

--- helpers/archiveUpgrade.sql
+++ helpers/archiveUpgrade.sql
@@ -1,6 +1,15 @@
+USE mannhullet_no;
+
+-- Change the name of the document collection to 'documents' from 'nths'
+UPDATE FileCollection SET collection = 'documents' WHERE collection = 'nths';
+UPDATE FileCollection SET src = REPLACE(src, '/uploads/nths/', '/uploads/documents/') WHERE collection = 'documents';
+
+-- Create a folder 'Filer'
 INSERT INTO FileCollection (title, category, collection, created)
 VALUES ('Filer', '2017/2018', 'documents', UNIX_TIMESTAMP());
 
+-- Move all documents to be Files instead of FileCollections
+-- Places all files in a singe folder 'Filer', need to be moved manually later
 INSERT INTO File (title, created, updated, src, collection_id)
 SELECT f1.title, f1.created, f1.updated, f1.src, f2.id
 FROM FileCollection f1, FileCollection f2
--- helpers/nthsToDocuments.sql
+++
@@ -1,3 +0,0 @@
-USE mannhullet_no;
-UPDATE FileCollection SET collection = 'documents' WHERE collection = 'nths';
-UPDATE FileCollection SET src = REPLACE(src, '/uploads/nths/', '/uploads/documents/') WHERE collection = 'documents';
