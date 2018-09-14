9c1e1ca42d37e102466abf1bda6e242ea787a6e3
jonmmo <jonmmo@stud.ntnu.no>
2018-09-14 23:36:08 +0200

    Added sql file to make a table with sjiraffenverses

---
+++ helpers/sjiraffenTable.sql
@@ -0,0 +1,34 @@
+USE mannhullet_no;
+CREATE TABLE sjiraffenvers (
+  id int NOT NULL AUTO_INCREMENT,
+  tekst varChar(255),
+  dato varChar(255),
+  kommentar varChar(255),
+  PRIMARY KEY(id)
+);
+
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jeg ønsker meg en himmelseng<br/>en himmelseng med speil i<br/>så jeg kan ligge og speile meg<br/>og se hvor jeg er deilig.' , 'Originalt vers');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Sjiraffen den har fire ben<br/>mannen han har två<br/>Kuken min har ingen ben<br/>men den kan enda stå.' , 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Hesten den har fire ben<br/>og eselet har mule<br/>alle jenter gleder<br/>når de skal ut og reise til Amerika.' , 'Originalt vers');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Kongen stod og vaska seg<br/>og vannet det var lunka<br/>det som ingen visste var<br/>at kongen stod og...' , 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('En gang jeg var så kåt så kåt<br/>jeg putta den i kverna<br/>men da jeg trakk den ut igjen<br/>var hele hodet fjerna.' , 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jag hoppade ur Lundsbrua<br/>trodde det var Whisky<br/>men det var inte det det var<br/>för da va vann med fisk i' , 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jag träffade en tjei i kveld<br/>men tjeien ville inte<br/>da reiste jeg til Afrika<br/>och knulla Konta-Kinte.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jag träffade en tjei i kveld<br/>men tjeiet falt i Nilen<br/>men det gjør ingen verdens ting<br/>för jag er nekrofilen.' ,'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jag träffade en tjei i kveld<br/>men tjeiet falt i diket<br/>det gjør ingen verdens ting<br/>för jag kan knulla liket.' , 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jag träffade en tjei i kveld<br/>och tjeien gav mej whisky<br/>men det var inte det det var<br/>for det var vann med piss i.' , 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jag träffade en tjei i kveld<br/>men tjeien begynnte vråla<br/>det gjör ingen verdens ting<br/>for det låter just som Carola!', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jeg drakk min egen sæd i kveld<br/>og havna på klinikken<br/>legen sa: Det va''kke bra!<br/>og amputerte pikken.' ,'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jeg fant ei flaske sprit i dag<br/>men flaska glapp av neven<br/>jeg var like glad for det<br/>men gladest var nok lever''n. ', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jeg fant en flaske øl i går<br/>men øllen gikk i bakken<br/>jeg leita hele dagen<br/>til jeg ble stiv i nakken.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jeg drakk en flaske vin i går<br/>men vinen gikk i nesen<br/>men jeg ble like full for det<br/>for jeg er ikke kresen.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('En murer stod på et stilas<br/>klødde seg på sin isse<br/>da lød det høyt der oppe fra:<br/>Flætt dæ æ må tisse.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jeg kjøpte meg et pornoblad<br/>og slo det opp på mitten<br/>og vet du hva jeg så i hu?<br/>jeg så hun midt i fitten.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Hora hette Tore<br/>han snudde seg og gapa<br/>så forhuden ble fylt med luft<br/>når Tore satt og rapa.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('I Trondheim festes det jo hardt<br/>og leses litt på matte<br/>bøgene i Oslo-by<br/>de drikker caffe latte.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('I Trondheim så studeres det<br/>her skapes ingeniører<br/>i Oslo slås og voldtas det<br/>det er ihvertfall det vi hører.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('I Oslo har de advokat<br/>og narkiser på plata<br/>dette danner grunnlaget<br/>for horene på gata.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('I Trondheim drikkes det mer øl<br/>enn det er vann i Themsen<br/>Oslojussen er så femi<br/>at selv gutta der får mensen.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jentene i åsblæsten<br/>de er så jævlig stygge<br/>de jager oss i Oslo, men<br/>i Trondheim er vi trygge.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Samfundet i Ås<br/>skulle være for oss alle<br/>men det som ingen visste<br/>var at Samfundet var en balle.', 'Hentet fra Tåkelurenheftet');
+INSERT INTO sjiraffenvers(tekst, kommentar) VALUES ('Jeg datet ei på åsblæsten<br/>vi begynte litt å deale<br/>jeg tok ei hagle og drepte ho<br/>for jeg er NEKROFIL''E!!!','Hentet fra Tåkelurenheftet');
